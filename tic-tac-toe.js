29f063387b8db7ad2330f3c4e21691446cd96cf2
Tekki999 <deneton.wright12@gmail.com>
2024-10-27 12:44:48 -0500

    Exercise 4

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,20 +2,46 @@ document.addEventListener("DOMContentLoaded", () => {
     const squares = document.querySelectorAll("#board, > div");
     let currentPlayer = 'X';
     const gameState = Array(9).fill(null);
+    const statusDiv = document.getElementById('status');
+
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    function checkForWinner() {
+        for (const combination of winningCombinations){
+            const [a ,b, c] = combination;
+
+            if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
+                statusDiv.textContent = 'Congratulations! ${gameState[a]} is the Winner!';
+                statusDiv.classList.add('you-won');
+                return true;
+            }
+        }
+        return false;
+    }
 
     function handleSquareClick(event) {
         const square = event.target;
         const index = Array.from(squares).indexOf(square);
 
-        if (gameState[index]) return;
+        if (gameState[index] || statusDiv.classList.contains('you-won')) return;
 
         square.textContent = currentPlayer;
         square.classList.add(currentPlayer);
         gameState[index] = currentPlayer;
 
-        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        if (!checkForWinner()) {
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';    
+        }
     }
-
     function handleMouseOver(event) {
         const square = event.target;
         if (!square.textContent) {
